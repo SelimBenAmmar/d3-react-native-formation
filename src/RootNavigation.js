1b7024aeb46fc03ee5805503b778bc4d342d8340
SelimBenAmmar <selim.benammar@telecom-paristech.fr>
2018-01-07 14:46:58 +0100

    Add react-native -svg and add graph with view in absolute position

--- src/RootNavigation.js
+++ src/RootNavigation.js
@@ -1,13 +1,19 @@
 // @flow
 
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator } from "react-navigation";
 
-import * as Pages from 'd3formation/src/pages';
+import * as Pages from "d3formation/src/pages";
 
-export default StackNavigator({
-  home: {
-    screen: Pages.Home,
+export default StackNavigator(
+  {
+    home: {
+      screen: Pages.Home,
+      navigationOptions: {
+        header: null
+      }
+    }
   },
-}, {
-  initialRouteName: 'home',
-});
+  {
+    initialRouteName: "home"
+  }
+);
--- src/pages/Home/Home.component.js
+++ src/pages/Home/Home.component.js
@@ -1,15 +1,14 @@
 // @flow
 
 import React, { Component } from "react";
-import { Text, View, Dimensions, ART } from "react-native";
+import { Text, View, Dimensions, StyleSheet, ART } from "react-native";
 import * as d3Scale from "d3-scale";
 import * as d3Shape from "d3-shape";
+import * as d3Axis from "d3-axis";
 import differenceInDays from "date-fns/difference_in_days";
 
 import { Page } from "d3formation/src/components";
 import data from "d3formation/src/data/data.json";
-
-import styles from "./Home.style";
 
 const { Surface, Group, Shape } = ART;
 
@@ -17,36 +16,61 @@ const interpersonalSkill = data.skills.interpersonal;
 const numberActivites = interpersonalSkill.length - 1;
 const firstDate = interpersonalSkill[0].date;
 const lastDate = interpersonalSkill[numberActivites].date;
+const XDomain = [0, differenceInDays(lastDate, firstDate)];
 
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 
 export default class Home extends Component {
   setTimeScale = dateValue => {
-    const Xaxis = [0, differenceInDays(lastDate, firstDate)];
-
     const x = d3Scale
       .scaleTime()
-      .domain(Xaxis)
-      .range([0, screenWidth]);
-    return x(differenceInDays(dateValue, firstDate));
+      .domain(XDomain)
+      .range([0, screenWidth / 2]);
+    return x(dateValue);
   };
 
   setSkillScale = skillValue => {
     const y = d3Scale
       .scaleLinear()
       .domain([0, 20])
-      .range([0, screenHeight]);
+      .range([screenHeight / 2, 0]);
     return y(skillValue);
   };
 
   setShape = data => {
     const line = d3Shape
       .line()
-      .x(d => this.setTimeScale(d.date))
+      .x(d => this.setTimeScale(differenceInDays(d.date, firstDate)))
       .y(d => this.setSkillScale(d.value))
       .curve(d3Shape.curveCatmullRom.alpha(0.5));
     return line(data);
+  };
+
+  getAxisTicks = data => {
+    const ticks = data.skills.interpersonal.map(datum => ({
+      x: this.setTimeScale(differenceInDays(datum.date, firstDate)),
+      y: this.setSkillScale(datum.value)
+    }));
+    return ticks;
+  };
+
+  displayAxis = data => {
+    return (
+      <View style={styles.xAxisContainer}>
+        <View style={styles.xAxisline} />
+        <View style={{ flexDirection: "row" }}>
+          {this.getAxisTicks(data).map((tick, index) => {
+            return (
+              <View key={index}>
+                <View style={styles.xAxisTicks} />
+                <Text style={styles.text}>{Math.round(tick.x)}</Text>
+              </View>
+            );
+          })}
+        </View>
+      </View>
+    );
   };
 
   render() {
@@ -56,12 +80,40 @@ export default class Home extends Component {
           <Group x={100} y={0}>
             <Shape
               d={this.setShape(data.skills.interpersonal)}
-              stroke="#000"
-              strokeWidth={1}
+              stroke="gray"
+              strokeWidth={2}
+            />
+            <Shape
+              d={this.setShape(data.skills.verbal_linguistic)}
+              stroke="red"
+              strokeWidth={2}
+            />
+            <Shape
+              d={this.setShape(data.skills.naturalistic)}
+              stroke="green"
+              strokeWidth={2}
             />
           </Group>
         </Surface>
+        {this.displayAxis(data)}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  xAxisContainer: {
+    position: "absolute",
+    top: 284,
+    left: 99,
+    flexDirection: "column",
+    justifyContent: "space-between"
+  },
+  xAxisline: { height: 2, width: 170, backgroundColor: "black" },
+  xAxisTicks: {
+    height: 10,
+    width: 1,
+    backgroundColor: "black"
+  },
+  text: { paddingRight: 10 }
+});
